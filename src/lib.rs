795f75602f4469f1abb5b8039fa687b1952511d8
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2018-07-28 11:10:29 +0300

    Changing type i64 to u64 since fibonacci deals only wih positive numbers
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
 
-pub fn fibonacci_with_hashmap_memoization(n: i64, cache: &mut HashMap<i64, i64>) -> i64 {
+pub fn fibonacci_with_hashmap_memoization(n: u64, cache: &mut HashMap<u64, u64>) -> u64 {
     assert!(n > 0, true);
 
     if cache.contains_key(&n) {
-        let result: &i64 = cache.get(&n).unwrap();
+        let result: &u64 = cache.get(&n).unwrap();
         return *result;
     }
 
@@ -12,17 +12,17 @@ pub fn fibonacci_with_hashmap_memoization(n: i64, cache: &mut HashMap<i64, i64>)
         return 1;
     }
 
-    let result: i64 = fibonacci_with_hashmap_memoization(n - 1, cache) + fibonacci_with_hashmap_memoization(n - 2, cache);
+    let result: u64 = fibonacci_with_hashmap_memoization(n - 1, cache) + fibonacci_with_hashmap_memoization(n - 2, cache);
 
     cache.insert(n, result);
 
     return result;
 }
 
-pub fn fibonacci_with_array_memoization(n: i64, cache: &mut [i64]) -> i64 {
+pub fn fibonacci_with_array_memoization(n: u64, cache: &mut [u64]) -> u64 {
     assert!(n > 0, true);
     let index: usize = n as usize;
-    let array_value: i64 = cache[index];
+    let array_value: u64 = cache[index];
 
     if array_value > 0 {
         return array_value;
@@ -32,20 +32,20 @@ pub fn fibonacci_with_array_memoization(n: i64, cache: &mut [i64]) -> i64 {
         return 1;
     }
 
-    let result: i64 = fibonacci_with_array_memoization(n - 1, cache) + fibonacci_with_array_memoization(n - 2, cache);
+    let result: u64 = fibonacci_with_array_memoization(n - 1, cache) + fibonacci_with_array_memoization(n - 2, cache);
 
     cache[index] = result;
 
     return result;
 }
 
-pub fn fibonacci_iterative(n: i64) -> i64 {
+pub fn fibonacci_iterative(n: u64) -> u64 {
     assert!(n > 0, true);
     if n == 1 || n == 2 { return 1; }
 
-    let mut current: i64 = 0;
-    let mut previous: i64 = 1;
-    let mut previous2: i64 = 1;
+    let mut current: u64 = 0;
+    let mut previous: u64 = 1;
+    let mut previous2: u64 = 1;
 
     for _i in 3..n + 1 {
         current = previous + previous2;
--- tests/tests.rs
+++ tests/tests.rs
@@ -4,7 +4,7 @@ extern crate fibonacci;
 
 #[test]
 fn test_fibonacci_with_hashmap_memoization() {
-    let mut hash_map: HashMap<i64, i64> = HashMap::new();
+    let mut hash_map: HashMap<u64, u64> = HashMap::new();
 
     assert_eq!(fibonacci::fibonacci_with_hashmap_memoization(3, &mut hash_map), 2);
     assert_eq!(fibonacci::fibonacci_with_hashmap_memoization(5, &mut hash_map), 5);
@@ -14,7 +14,7 @@ fn test_fibonacci_with_hashmap_memoization() {
 
 #[test]
 fn test_fibonacci_with_array_memoization() {
-    let mut array: [i64; 47] = [0; 47];
+    let mut array: [u64; 47] = [0; 47];
 
     assert_eq!(fibonacci::fibonacci_with_array_memoization(3, &mut array), 2);
     assert_eq!(fibonacci::fibonacci_with_array_memoization(5, &mut array), 5);
